2027213b377b8d32316098d973558407299c5ac2
goobman11 <116324435+goobman11@users.noreply.github.com>
2022-10-21 11:42:40 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { Goob } from "./goob/Goob";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello I am Goob</h1>
+      <h2>My eimenes are tring to take over da erth</h2>
+      <h2>Peas help me </h2>
+      <Goob />
+    </div>
+  );
+}
---
+++ src/goob/Goob.tsx
@@ -0,0 +1,12 @@
+export const Goob = () => {
+  return (
+    <svg height="300" width="300">
+      <ellipse rx="30" ry="60" cx="100" cy="100" fill="#c4ff0e" />{" "}
+      <ellipse rx="30" ry="60" cx="150" cy="100" fill="#c4ff0e" />
+      <ellipse rx="30" ry="60" cx="200" cy="100" fill="#c4ff0e" />
+      <rect x="70" fill="#c4ff0e" y="100" height="100" width="160" />
+      <path d="M70 200 L 70 90 Z" stroke="#000000" />
+      <circle> fill="#000000" rx="30" ry="60 cx="100" cy="100" />
+    </svg>
+  );
+};
